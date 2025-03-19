919315220e71ed3e11f63ae1fbeb888f1683f1e6
charlesremy <charles.remy@my-digital-school.org>
2025-03-19 16:32:04 +0100

    remove noIndex

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,7 +26,7 @@ const config = {
   organizationName: "facebook", // Usually your GitHub org/user name.
   projectName: "docusaurus", // Usually your repo name.
 
-  noIndex: true, // Defaults to `false`
+  noIndex: false, // Defaults to `false`
 
   onBrokenLinks: "ignore",
   onBrokenMarkdownLinks: "ignore",
